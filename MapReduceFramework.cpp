c27f924156a60e4c205f2dce2df18cb085afeaf2
ophirhan <ophir.han@mail.huji.ac.il>
2020-05-24 20:44:47 +0300

    todo update mutex percentage

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -28,16 +28,10 @@ typedef std::pair<threadVector*, pthread_mutex_t*> threadVectorAndMutex;
  * @param multiThreadLevel
  * @return
  */
-typedef struct threadData{
-
-    // the input vector
-    const InputVec* inputVec;///
+typedef struct {
 
     //The mapreduceclient
     const MapReduceClient* client;
-
-    // check progress on input vector
-    std::atomic<int>* inputCounter;///
 
     // how many threads have finished map phase
     std::atomic<int>* mapBarrier;
@@ -45,68 +39,98 @@ typedef struct threadData{
     // is shuffle phase over?
     pthread_cond_t* shuffleDone;
 
-    // signal shuffle new pairs are available
-    pthread_cond_t* shuffleResume;
+    // is shuffle phase over?
+    pthread_mutex_t* shuffleDoneMutex;
+
+    // is shuffle phase over?
+    pthread_mutex_t* outVecMutex;
 
     // how many pairs are available for shuffle to process
     std::atomic<int>* pairCount;
-}threadData;
 
-typedef struct mapThread{
+    // how many pairs are available for shuffle to process
+    std::atomic<int>* reduceCounter;
 
-    //common data map threads need
-    threadData* data;
+    // map of key = k2*, value = V2*
+    IntermediateMap* inter;
+
+    // map of key = k2*, value = V2*
+    std::vector<K2*> interKeys;
+
+    //output vec
+    OutputVec* out;
+
+    //jobstate
+    JobState* jobState;
+
+} JobContext;
+
+typedef struct{
+
+    // the input vector
+    const InputVec* inputVec;
+
+    //common job map threads need
+    JobContext* job;
 
     // personal output vec for mapthread
     threadVectorAndMutex tvAndMutex;
 
     // thread id
     int threadID;
+
+    // check progress on input vector
+    std::atomic<int>* inputCounter;
+
 } mapThread;
 
-typedef struct shuffleThread{
+typedef struct{
 
     // all personal output thread vectors
-    std::vector<std::pair<threadVector*, pthread_mutex_t*>> threadVectors;////
-
-    // how many pairs are available for shuffle
-    std::atomic<int>* pairCount;
-
-    // the client
-    const MapReduceClient* client;
-
-    // how many map threads done
-    std::atomic<int>* mapBarrier;
-
-    // sig shuffle to resume
-    pthread_cond_t* shuffleResume;
-
-    // shuffle phase over
-    pthread_cond_t* shuffleDone;
+    std::vector<std::pair<threadVector*, pthread_mutex_t*>> threadVectors;
 
     // how many threads do we have
     int multiThreadLevel;
 
     // shuffle thread id
     int threadID;
+
+    //common job map threads need
+    JobContext* data;
 } shuffleThread;
 
-void* threadLife(void *context){
-    auto *tData = (mapThread*)context;
-    int old_value = (*tData->data->inputCounter)++;
-    while(old_value < tData->data->inputVec->size()) {
-        tData->data->client->map(tData->data->inputVec->at(old_value).first, tData->data->inputVec->at(old_value).second, context);
+void reduceWrapper(void* context){
+    auto* tJob = (JobContext*) context;
+    int oldValue = (*tJob->reduceCounter)++;
+    while(oldValue < tJob->interKeys.size())
+    {
+        K2* key = tJob->interKeys.at(oldValue);
+        tJob->client->reduce(key, tJob->inter->at(key), context);
     }
-    (*tData->data->mapBarrier)++;
-    waitForJob(tData->data->shuffleDone); // todo find shuffle jobhandle
-    //    tData->data->client->reduce(tData->data->inputVec->at(old_value).first, tData->data->inputVec->at(old_value).second, context);
+}
+
+void* mapNreduce(void *context){
+    auto *tData = (mapThread*)context;
+    int old_value = (*tData->inputCounter)++;
+    while(old_value < tData->inputVec->size()) {
+        tData->job->client->map(tData->inputVec->at(old_value).first, tData->inputVec->at(old_value).second, context);
+    }
+    (*tData->job->mapBarrier)++;
+    //waitForJob(tData->job->shuffleDone); // todo find shuffle jobhandle
+    if(pthread_mutex_lock(tData->job->shuffleDoneMutex) != 0){
+        //todo
+        //exit()
+    }
+    pthread_cond_wait(tData->job->shuffleDone, tData->job->shuffleDoneMutex);
+    pthread_mutex_unlock(tData->job->shuffleDoneMutex);
+    reduceWrapper(tData->job);
+    //    tData->job->client->reduce(tData->job->inputVec->at(old_value).first, tData->job->inputVec->at(old_value).second, context);
     // const K1* key, const V1* value, void* context
-    return nullptr;
+    return context;
 }
 
 JobHandle shuffle(void* context){
     auto sData = (shuffleThread*) context;
-    auto* intermediateMap = new std::map< K2*, std::vector<V2*>*, K2PointerComp>;
     int mapThreadCount = sData->multiThreadLevel - 1;
     bool done = false;
     while(!done)
@@ -116,31 +140,38 @@ JobHandle shuffle(void* context){
             threadVector* curVec = sData->threadVectors.at(i).first;
             pthread_mutex_t* mutex = sData->threadVectors.at(i).second;
             pthread_mutex_lock(mutex);
+            IntermediateMap* intermediateMap = sData->data->inter;
             while(!curVec->empty())
             {
                 IntermediatePair pair  = curVec->at(0);
                 curVec->erase(curVec->begin());
                 if(intermediateMap->find(pair.first) == intermediateMap->end())
                 {
-                    intermediateMap->at(pair.first) = new std::vector<V2*>;
+                    intermediateMap->at(pair.first) = std::vector<V2*>();
+                    sData->data->interKeys.push_back(pair.first);
                 }
-                intermediateMap->at(pair.first)->push_back(pair.second);
+                intermediateMap->at(pair.first).push_back(pair.second);
+                --(*sData->data->pairCount);
             }
             pthread_mutex_unlock(mutex);
         }
-        if(*sData->pairCount == 0)
+        if(*sData->data->mapBarrier == mapThreadCount)
         {
-            if(*sData->mapBarrier < mapThreadCount)
-            {
-                // what if here last signal happens
-                pthread_cond_wait(sData->shuffleResume, nullptr);
-            }
-            else if (*sData->pairCount == 0) // make sure
+            sData->data->jobState->stage = SHUFFLE_STAGE;
+            if (*sData->data->pairCount == 0)
             {
                 done = true;
             }
         }
     }
+
+    if(pthread_mutex_lock(sData->data->shuffleDoneMutex) != 0){
+        //todo
+        //exit()
+    }
+    pthread_cond_broadcast(sData->data->shuffleDone);
+    pthread_mutex_unlock(sData->data->shuffleDoneMutex);
+    reduceWrapper(sData->data);
     return nullptr;
 }
 
@@ -152,49 +183,61 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
     std::atomic<int> inputCounter(0);
     std::atomic<int> mapBarrier(0);
     std::atomic<int> pairCount(0);
-    auto *data = new threadData;
+    std::atomic<int> reduceCounter(0);
+    std::atomic<int> processedK1(0);
+    auto* data = new JobContext;
+    auto* jobState = new JobState;
+    jobState->stage = UNDEFINED_STAGE;
+    jobState->percentage = 0.0;
+    IntermediateMap a;
+    data->inter = &a;
     data->client = &client;
-    data->inputCounter = &inputCounter;
     data->mapBarrier = &mapBarrier;
     data->pairCount = &pairCount;
-    data->inputVec = &inputVec;
-    pthread_cond_t *shuffleDone = nullptr;// = PTHREAD_COND_INITIALIZER;
-    pthread_cond_t* shuffleResume = nullptr;// = PTHREAD_COND_INITIALIZER;
-    pthread_cond_init(shuffleDone, nullptr);
-    pthread_cond_init(shuffleResume, nullptr);
-    data->shuffleDone = shuffleDone;
-    data->shuffleResume = shuffleResume;
+    data->reduceCounter = &reduceCounter;
+    data->interKeys = std::vector<K2*>();
+    data->jobState = jobState;
+    pthread_cond_t shuffleDone = PTHREAD_COND_INITIALIZER;
+    pthread_mutex_t shuffDone = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_t outVecMutex = PTHREAD_MUTEX_INITIALIZER;
+    data->shuffleDone = &shuffleDone;
+    data->shuffleDoneMutex = &shuffDone;
+    data->outVecMutex = &outVecMutex;
+    data->out = &outputVec;
+    jobState->stage = MAP_STAGE;
     for(int i = 0; i < multiThreadLevel - 1; ++i){
         mapThread myData;
         myData.threadID = i;
         threadVector myVec;
-        pthread_mutex_t* vecMutex;
-        myData.tvAndMutex = std::make_pair(&myVec, vecMutex);
+        myData.inputCounter = &inputCounter;
+        myData.inputVec = &inputVec;
+        pthread_mutex_t vecMutex = PTHREAD_MUTEX_INITIALIZER;
+        myData.tvAndMutex = std::make_pair(&myVec, &vecMutex);
         threadVectors.push_back(myData.tvAndMutex);
-        myData.data = data;
-        pthread_create(threads + i, nullptr, threadLife, &myData);
+        myData.job = data;
+        pthread_create(threads + i, nullptr, mapNreduce, &myData);
     }
     shuffleThread shuffi;
-    shuffi.client = &client;
     shuffi.threadVectors = threadVectors;
-    shuffi.mapBarrier = &mapBarrier;
     shuffi.threadID = multiThreadLevel - 1;
     shuffi.multiThreadLevel = multiThreadLevel;
-    shuffi.pairCount = &pairCount;
-    shuffi.shuffleResume = shuffleResume;
+    shuffi.data = data;
     pthread_create(threads + (multiThreadLevel - 1), nullptr, shuffle, &shuffi);
+    return nullptr;/// todo Jobhandle
 }
 
 void waitForJob(JobHandle job){
-    auto* cvAndMutex = (std::pair<pthread_cond_t*, pthread_mutex_t*>*) job;
+    auto* cvAndMutex = (std::pair<pthread_cond_t*, pthread_mutex_t*>*) job; // pointer to pair
     pthread_mutex_lock(cvAndMutex->second);
     pthread_cond_wait(cvAndMutex->first, cvAndMutex->second);
     //work
     //unlock
 }
+
 void getJobState(JobHandle job, JobState* state){
 
 }
+
 void closeJobHandle(JobHandle job){
     //unlock
 
@@ -205,10 +248,13 @@ void emit2 (K2* key, V2* value, void* context){
     pthread_mutex_lock(tData->tvAndMutex.second);
     tData->tvAndMutex.first->push_back(std::make_pair(key,value));
     pthread_mutex_unlock(tData->tvAndMutex.second);
-    (*tData->data->pairCount)++;
-    pthread_cond_signal(tData->data->shuffleResume);
+    ++(*tData->job->pairCount);
+//    tData->job->jobState->percentage =
 }
 
 void emit3 (K3* key, V3* value, void* context){
-
+    auto* tData = (JobContext*) context;
+    pthread_mutex_lock(tData->outVecMutex);
+    tData->out->push_back(std::make_pair(key, value));
+    pthread_mutex_unlock(tData->outVecMutex);
 }
