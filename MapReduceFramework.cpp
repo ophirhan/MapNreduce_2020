bda85f7d897f885a92b42e468584fa2873d0644a
ophirhan <P0k$n123>
2020-06-01 17:50:12 +0300

    Half documentation left

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -30,25 +30,22 @@ typedef std::vector<std::pair<K2*,V2*>> threadVector;
  */
 typedef struct {
 
-    // is shuffle phase over?
-    bool doneJob;
-
-    //The mapreduceclient
+    // The mapReduceClient
     const MapReduceClient* client;
 
     // how many threads have finished map phase
-    //std::atomic<int>* mapBarrier;
     Barrier* mapBarrier;
 
+    // Array of threads
     pthread_t* threads;
 
-    // how many threads have finished map phase
+    // counter of processed keys in curent phase.
     std::atomic<int>* processedKeys;
 
-    // is shuffle phase over?
+    // pointer to the output vector mutex
     pthread_mutex_t* outVecMutex;
 
-    // is shuffle phase over?
+    // Mutex of jobstate
     pthread_mutex_t* jobStateMutex;
 
     // is shuffle phase over?
@@ -57,17 +54,17 @@ typedef struct {
     // how many pairs are available for shuffle to process
     std::atomic<int>* pairCount;
 
-    // how many pairs are available for shuffle to process
+    // how many pairs are available to map or reduce.
     std::atomic<unsigned long>* workCounter;
 
     // map of key = k2*, value = V2*
     IntermediateMap* inter;
 
-    // map of key = k2*, value = V2*
+    // vector of unique keys to processes
     std::vector<K2*>* interKeys;
 
     //output vec
-    OutputVec* out;
+    OutputVec* outputVec;
 
     //jobstate
     JobState* jobState;
@@ -75,8 +72,8 @@ typedef struct {
     // how many threads do we have
     int multiThreadLevel;
 
-    // atomic bool
-    std::atomic_bool* done;
+    // Has someone called wait
+    std::atomic_bool* someoneWaiting;
 
 } JobContext;
 
@@ -91,6 +88,7 @@ typedef struct{
     // personal output vec for mapthread
     threadVector* tv;
 
+    // mutex of threadVector
     pthread_mutex_t* tvMutex;
 
 
@@ -102,13 +100,30 @@ typedef struct{
     // all personal output thread vectors
     std::vector<threadVector*>* threadVectors;
 
-    // all personal output thread vectors
+    // all personal output thread vectors mutexes
     std::vector<pthread_mutex_t*>* threadMutexes;
 
-    //common job map threads need
+    //common job struct
     JobContext* data;
 } shuffleThread;
 
+void finishShuffle(const shuffleThread *sData, int mapThreadCount);
+
+float processVector(const shuffleThread *sData, float shuffleCount, float totalJob,
+                    IntermediateMap *intermediateMap, int treadNum);
+
+float setShuffle(const shuffleThread *sData, float shuffleCount, float totalJob);
+
+JobContext *
+initJobContext(const MapReduceClient &client, OutputVec &outputVec, int multiThreadLevel,
+               pthread_t *threads);
+
+mapThread *initMapThreadData(const InputVec &inputVec, JobContext *data);
+
+/**
+ * This function is responsible to find next available key to reduce and call reduce upon it.
+ * @param context void* (JobContext*) pointer to struct containing needed resources.
+ */
 void reduceWrapper(void* context){
     auto* tJob = (JobContext*) context;
     unsigned long oldValue = (*tJob->workCounter)++;
@@ -130,6 +145,12 @@ void reduceWrapper(void* context){
     }
 }
 
+/**
+ * This function is responsible to find next available key to map and call map upon it.
+ * after no more keys are available to map, waits for barrier and sends the thread to reduce phase.
+ * @param context context void* (JobContext*) pointer to struct containing needed resources.
+ * @return JobContext
+ */
 void* mapNreduce(void *context){
     auto *tData = (mapThread*) context;
     unsigned long old_value = (*tData->job->workCounter)++;
@@ -158,6 +179,12 @@ void* mapNreduce(void *context){
     return context;
 }
 
+/**
+ * Iterates through all private thread vectors and sorts their content into an intermediate map.
+ * after that 
+ * @param context 
+ * @return 
+ */
 JobHandle shuffle(void* context){
     auto* sData = (shuffleThread*) context;
     int mapThreadCount = sData->data->multiThreadLevel - 1;
@@ -169,63 +196,110 @@ JobHandle shuffle(void* context){
     {
         for(int i = 0; i < mapThreadCount; ++i)
         {
-            threadVector* curVec = sData->threadVectors->at(i);
-            pthread_mutex_t* mutex = sData->threadMutexes->at(i);
-            if(pthread_mutex_lock(mutex) != 0){
-                fprintf(stderr, "system error: error on pthread_mutex_lock\n");
-                exit(1);
-            }
-            while(!curVec->empty())
-            {
-                IntermediatePair pair  = curVec->at(0);
-                curVec->erase(curVec->begin());
-                if(intermediateMap->find(pair.first) == intermediateMap->end())
-                {
-                    intermediateMap->insert(std::make_pair(pair.first,std::vector<V2*>()));
-                    sData->data->interKeys->push_back(pair.first);
-                }
-                intermediateMap->at(pair.first).push_back(pair.second);
-                --(*sData->data->pairCount);
-                shuffleCount++;
-            }
-            if(pthread_mutex_unlock(mutex) != 0){
-                fprintf(stderr, "system error: error on pthread_mutex_unlock\n");
-                exit(1);
-            }
-            if(pthread_mutex_lock(sData->data->jobStateMutex) != 0){
-                fprintf(stderr, "system error: error on pthread_mutex_lock\n");
-                exit(1);
-            }
-            if(sData->data->jobState->stage == SHUFFLE_STAGE)
-            {
-                sData->data->jobState->percentage = PERCENTAGE * (shuffleCount / totalJob);
-            }
-            if(pthread_mutex_unlock(sData->data->jobStateMutex) != 0){
-                fprintf(stderr, "system error: error on pthread_mutex_unlock\n");
-                exit(1);
-            }
+            shuffleCount = processVector(sData, shuffleCount, totalJob, intermediateMap, i);
         }
         int count;
         sData->data->mapBarrier->getCount(&count);
         if(count == mapThreadCount)
         {
-            if(pthread_mutex_lock(sData->data->jobStateMutex) != 0){
-                fprintf(stderr, "system error: error on pthread_mutex_lock\n");
-                exit(1);
-            }
-            sData->data->jobState->stage = SHUFFLE_STAGE;
-            totalJob = shuffleCount + *sData->data->pairCount;
-            sData->data->jobState->percentage = PERCENTAGE * (shuffleCount / totalJob);
-            if(pthread_mutex_unlock(sData->data->jobStateMutex) != 0){
-                fprintf(stderr, "system error: error on pthread_mutex_unlock\n");
-                exit(1);
-            }
+            totalJob = setShuffle(sData, shuffleCount, totalJob);
             if (*sData->data->pairCount == 0)
             {
                 done = true;
             }
         }
     }
+
+    finishShuffle(sData, mapThreadCount);
+
+    sData->data->mapBarrier->barrier();
+    JobContext* job = sData->data;
+    delete sData;
+    reduceWrapper(job);
+    return nullptr;
+}
+
+/**
+ * Sets the stage to shuffle and updates the percentage accordingly.
+ * @param sData struct with all resources.
+ * @param shuffleCount count of keys that shuffle sorted
+ * @param totalJob total count of k2 keys shuffle has to process
+ * @return totalJob
+ */
+float setShuffle(const shuffleThread *sData, float shuffleCount, float totalJob)
+{
+    if(pthread_mutex_lock(sData->data->jobStateMutex) != 0){
+        fprintf(stderr, "system error: error on pthread_mutex_lock\n");
+                exit(1);
+    }
+    sData->data->jobState->stage = SHUFFLE_STAGE;
+    totalJob = shuffleCount + *sData->data->pairCount;
+    sData->data->jobState->percentage = PERCENTAGE * (shuffleCount / totalJob);
+    if(pthread_mutex_unlock(sData->data->jobStateMutex) != 0){
+        fprintf(stderr, "system error: error on pthread_mutex_unlock\n");
+                exit(1);
+    }
+    return totalJob;
+}
+
+/**
+ * sort the private vector into intermediate vector until no more keys are available.
+ * @param sData struct with common resources.
+ * @param shuffleCount total intermediate pairs keys that were sorted
+ * @param totalJob total pairs to process
+ * @param intermediateMap the output vec of shuffle
+ * @param treadNum current thread and vector num
+ * @return total pairs proccesed by shuffle.
+ */
+float processVector(const shuffleThread *sData, float shuffleCount, float totalJob,
+                    IntermediateMap *intermediateMap, int treadNum)
+{
+    threadVector* curVec = sData->threadVectors->at(treadNum);
+    pthread_mutex_t* curMutex = sData->threadMutexes->at(treadNum);
+    if(pthread_mutex_lock(curMutex) != 0){
+        fprintf(stderr, "system error: error on pthread_mutex_lock\n");
+                exit(1);
+    }
+    while(!curVec->empty())
+    {
+        IntermediatePair pair  = curVec->at(0);
+        curVec->erase(curVec->begin());
+        if(intermediateMap->find(pair.first) == intermediateMap->end())
+        {
+            intermediateMap->insert(std::make_pair(pair.first,std::vector<V2*>()));
+            sData->data->interKeys->push_back(pair.first);
+        }
+        intermediateMap->at(pair.first).push_back(pair.second);
+        --(*sData->data->pairCount);
+        shuffleCount++;
+    }
+    if(pthread_mutex_unlock(curMutex) != 0){
+        fprintf(stderr, "system error: error on pthread_mutex_unlock\n");
+                exit(1);
+    }
+
+    if(pthread_mutex_lock(sData->data->jobStateMutex) != 0){
+        fprintf(stderr, "system error: error on pthread_mutex_lock\n");
+                exit(1);
+    }
+    if(sData->data->jobState->stage == SHUFFLE_STAGE)
+    {
+        sData->data->jobState->percentage = PERCENTAGE * (shuffleCount / totalJob);
+    }
+    if(pthread_mutex_unlock(sData->data->jobStateMutex) != 0){
+        fprintf(stderr, "system error: error on pthread_mutex_unlock\n");
+                exit(1);
+    }
+    return shuffleCount;
+}
+
+/**
+ * finish shuffle set stage to reduce and init relevant structures.
+ * @param sData struct containg common resources
+ * @param mapThreadCount number of thread in map phase.
+ */
+void finishShuffle(const shuffleThread *sData, int mapThreadCount)
+{
     for(int i=0; i < mapThreadCount; ++i){
         delete sData->threadVectors->at(i);
         if(pthread_mutex_destroy(sData->threadMutexes->at(i)) != 0){
@@ -236,6 +310,7 @@ JobHandle shuffle(void* context){
     }
     delete sData->threadVectors;
     delete sData->threadMutexes;
+
     (*sData->data->workCounter) = 0;
     (*sData->data->processedKeys) = 0;
     if(pthread_mutex_lock(sData->data->jobStateMutex) != 0){
@@ -248,14 +323,17 @@ JobHandle shuffle(void* context){
         fprintf(stderr, "system error: error on pthread_mutex_unlock\n");
         exit(1);
     }
-
-    sData->data->mapBarrier->barrier();
-    JobContext* job = sData->data;
-    delete sData;
-    reduceWrapper(job);
-    return nullptr;
 }
 
+/**
+ * This function starts running the MapReduce algorithm (with several threads)
+ * and returns a JobHandle
+ * @param client containing map and reduce implemntations
+ * @param inputVec Given inputVec
+ * @param outputVec pointer to the vector to output results to
+ * @param multiThreadLevel desired number of threads to create for the job.
+ * @return JobHandle containing jobstate.
+ */
 JobHandle startMapReduceJob(const MapReduceClient& client,
                             const InputVec& inputVec, OutputVec& outputVec,
                             int multiThreadLevel){
@@ -264,9 +342,56 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
     auto* threadVectors = new std::vector<threadVector*>;
     auto threadMutexes = new std::vector<pthread_mutex_t*>;
 
+    JobContext *data = initJobContext(client, outputVec, multiThreadLevel, threads);
+
+
+    for(int i = 0; i < multiThreadLevel - 1; ++i){
+        mapThread *myData = initMapThreadData(inputVec, data);
+        threadVectors->push_back(myData->tv);
+        threadMutexes->push_back(myData->tvMutex);
+        if(pthread_create(threads + i, nullptr, mapNreduce, myData) != 0){
+            fprintf(stderr, "system error: error on pthread_create\n");
+            exit(1);
+        }
+    }
+    auto shuffleContext = new shuffleThread;
+    shuffleContext->threadVectors = threadVectors;
+    shuffleContext->threadMutexes = threadMutexes;
+    shuffleContext->data = data;
+    if(pthread_create(threads + (multiThreadLevel - 1),
+            nullptr, shuffle, shuffleContext) != 0){
+        fprintf(stderr, "system error: error on pthread_create\n");
+        exit(1);
+    }
+    return data;
+}
+
+mapThread *initMapThreadData(const InputVec &inputVec, JobContext *data)
+{
+    auto* myData = new mapThread;
+    myData->inputVec = &inputVec;
+    auto vecMutex = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
+    if(vecMutex == nullptr){
+        fprintf(stderr, "system error: error on malloc\n");
+            exit(1);
+    }
+    if(pthread_mutex_init(vecMutex, nullptr) != 0){
+        fprintf(stderr, "system error: error on pthread_mutex_init\n");
+            exit(1);
+    }
+    myData->tv = new threadVector();
+    myData->tvMutex = vecMutex;
+    myData->job = data;
+    return myData;
+}
+
+JobContext *
+initJobContext(const MapReduceClient &client, OutputVec &outputVec, int multiThreadLevel,
+               pthread_t *threads)
+{
     auto* data = new JobContext;
     data->jobState = new JobState;
-    data->out = &outputVec;
+    data->outputVec = &outputVec;
 
     data->threads = threads;
 
@@ -282,7 +407,7 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
     data->pairCount = new std::atomic<int>(0);
     data->processedKeys = new std::atomic<int>(0);
     data->workCounter = new std::atomic<unsigned long>(0);
-    data->done = new std::atomic_bool(false);
+    data->someoneWaiting = new std::atomic_bool(false);
 
 
     data->multiThreadLevel = multiThreadLevel;
@@ -320,50 +445,16 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
     }
     data->doneMutex = doneMutex;
 
-    data->doneJob = false;
-
     data->jobState->stage = MAP_STAGE;
-
-    for(int i = 0; i < multiThreadLevel - 1; ++i){
-        auto* myData = new mapThread;
-        myData->inputVec = &inputVec;
-        auto vecMutex = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
-        if(vecMutex == nullptr){
-            fprintf(stderr, "system error: error on malloc\n");
-            exit(1);
-        }
-        if(pthread_mutex_init(vecMutex, nullptr) != 0){
-            fprintf(stderr, "system error: error on pthread_mutex_init\n");
-            exit(1);
-        }
-        myData->tv = new threadVector();
-        myData->tvMutex = vecMutex;
-        threadVectors->push_back(myData->tv);
-        threadMutexes->push_back(myData->tvMutex);
-        myData->job = data;
-        if(pthread_create(threads + i, nullptr, mapNreduce, myData) != 0){
-            fprintf(stderr, "system error: error on pthread_create\n");
-            exit(1);
-        }
-    }
-    auto shuffleContext = new shuffleThread;
-    shuffleContext->threadVectors = threadVectors;
-    shuffleContext->threadMutexes = threadMutexes;
-    shuffleContext->data = data;
-    if(pthread_create(threads + (multiThreadLevel - 1),
-            nullptr, shuffle, shuffleContext) != 0){
-        fprintf(stderr, "system error: error on pthread_create\n");
-        exit(1);
-    }
     return data;
 }
 
 void waitForJob(JobHandle job){
     auto context = (JobContext*) job;
-    if(*context->done){
+    if(*context->someoneWaiting){
         return;
     }
-    *context->done= true;
+    *context->someoneWaiting= true;
     for(int i=0;i < context->multiThreadLevel; ++i){
         if(pthread_join(context->threads[i], nullptr) != 0){
             fprintf(stderr, "system error: error on pthread_join\n");
@@ -413,7 +504,7 @@ void closeJobHandle(JobHandle job){
     delete context->workCounter;
     delete context->jobState;
     delete context->mapBarrier;
-    delete context->done;
+    delete context->someoneWaiting;
     delete[] context->threads;
     delete context;
 }
@@ -439,7 +530,7 @@ void emit3 (K3* key, V3* value, void* context){
         fprintf(stderr, "system error: error on pthread_mutex_lock\n");
         exit(1);
     }
-    tData->out->emplace_back(key, value);
+    tData->outputVec->emplace_back(key, value);
     if(pthread_mutex_unlock(tData->outVecMutex) != 0){
         fprintf(stderr, "system error: error on pthread_mutex_unlock\n");
         exit(1);
