f93e939c1a8ac6db8275e69ae6cbe55114a62296
ophir <ophir.han@mail.huji.ac.il>
2020-06-22 15:35:54 +0300

    yes

--- cmake-build-debug/mattanTests/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cs/usr/ophirhan/CLionProjects/os_ex3_2020/cmake-build-debug/mattanTests/googletest-src/googlemock
-# Build directory: /cs/usr/ophirhan/CLionProjects/os_ex3_2020/cmake-build-debug/mattanTests/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
